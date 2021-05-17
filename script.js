2a147e11af6ad5fb71bc61c78271d4becc52deaa
toxmose <jeroen.vandroemme@gmail.com>
2021-05-17 23:33:30 +0200

    game logic added

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-'use strict';
+"use strict";
+
+let score = 20;
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = " 💩 Oops ! No number 💩";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = " 🎉 BINGO 🎉 ";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = " 🌡 Too high 🌡";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " 🙈 GAME OVER 🙈 ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = " ❄ Too low ❄ ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " 🙈 GAME OVER 🙈 ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  }
+});
