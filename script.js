aca71879bf921fb5b79d7bb4a8d1853ad2370a6b
toxmose <jeroen.vandroemme@gmail.com>
2021-05-18 23:58:11 +0200

    some refactoring

--- script.js
+++ script.js
@@ -2,46 +2,43 @@
 
 let score = 20;
 let secretNumber = Math.floor(Math.random() * 20) + 1;
+let highScore = 0;
+
+const displayMessage = (message) =>
+  (document.querySelector(".message").textContent = message);
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   // no input detected
   if (!guess) {
-    document.querySelector(".message").textContent = " 💩 Oops ! No number 💩";
+    displayMessage(" 💩 Oops ! No number 💩");
   }
 
   // guess is correct
   else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = " 🎉 BINGO 🎉 ";
+    displayMessage(" 🎉 BINGO 🎉 ");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     score--;
     document.querySelector(".score").textContent = score;
+
+    // highscore check
+    if (score > highScore) {
+      document.querySelector(".highscore").textContent = score;
+      highScore = score;
+    } else document.querySelector(".highscore").textContent = highScore;
   }
 
-  // guess is too high
-  else if (guess > secretNumber) {
+  // guess is wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = " 🌡 Too high 🌡";
+      displayMessage(guess > secretNumber ? " 🌡 Too high 🌡" : " ❄ Too low ❄ ");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = " 🙈 GAME OVER 🙈 ";
-      score--;
-      document.querySelector(".score").textContent = score;
-    }
-  }
-
-  // guess is too low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = " ❄ Too low ❄ ";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = " 🙈 GAME OVER 🙈 ";
+      displayMessage(" 🙈 GAME OVER 🙈 ");
       score--;
       document.querySelector(".score").textContent = score;
     }
@@ -49,12 +46,14 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 
 document.querySelector(".again").addEventListener("click", function () {
-  document.querySelector(".number").textContent = "?";
+  score = 20;
   secretNumber = Math.floor(Math.random() * 20) + 1;
+
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  displayMessage(" Start guessing... ");
+  document.querySelector(".guess").value = "";
+
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
-  score = 20;
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = " Start guessing... ";
-  document.querySelector(".guess").value = "";
 });
